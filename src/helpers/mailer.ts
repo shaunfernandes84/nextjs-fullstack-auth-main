a7d052e0183af9964cd7772c97f99bfc337c5ec3
Shaun Fernandes <145692712+shaunfernandes84@users.noreply.github.com>
2024-04-14 14:28:32 +0530

    Update mailer.ts

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -26,12 +26,12 @@ export const sendEmail = async({email, emailType, userId}:any) => {
           });
 
 
-        const mailOptions = {
+      const mailOptions = {
             from: 'shaunfernandes48@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
             html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
-            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifypassword?token=${hashedToken}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
             </p>`
         }
 
