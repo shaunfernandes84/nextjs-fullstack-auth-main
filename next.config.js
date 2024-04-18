404229893469fbc6ac6f803d8f1639f1990c70bf
Shaun Fernandes <145692712+shaunfernandes84@users.noreply.github.com>
2024-04-18 11:48:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    images: {
+      domains: ['www.capgemini.com', 'i.ytimg.com', 'getlucidity.com', 'assets.devx.work'], // Add your domains here
+    },
+  };
+  
